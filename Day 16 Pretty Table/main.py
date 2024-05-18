eb29e0ca39744710c752c59fd6348bbea5ddf631
AnilNAKR <n.anilkumar7896@gmail.com>
2024-05-19 00:26:10 +0530

    make change in Day 16 Pretty table project

--- Day 16 Pretty Table/main.py
+++ Day 16 Pretty Table/main.py
@@ -1,16 +1,3 @@
-# # import turtle
-# from turtle import Turtle, Screen
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("red")
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
-
-
 # Using PrettyTable for formating data inside a table.
 from prettytable import PrettyTable
 table = PrettyTable()
