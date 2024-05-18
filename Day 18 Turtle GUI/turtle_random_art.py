cee313abf32c9f8881b2f1711182b3c2648f6eb4
AnilNAKR <n.anilkumar7896@gmail.com>
2024-05-19 02:26:31 +0530

    make changes in Day 18 Turtle GUI Project

--- Day 18 Turtle GUI/turtle_random_art.py
+++ Day 18 Turtle GUI/turtle_random_art.py
@@ -11,9 +11,9 @@ directions = [0, 90, 180, 270]
 
 
 def random_color():
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
     random_color = (r, g, b)
     return random_color
 
