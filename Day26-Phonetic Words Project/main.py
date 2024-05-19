7d08edb0cf0ac6710c3851fedb0ecfcfc4f39993
AnilNAKR <n.anilkumar7896@gmail.com>
2024-05-19 14:47:21 +0530

    removed Day-26 Phonetic Words project folder

---
+++ Day26-Phonetic Words Project/main.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+#  Below code gives us phonetic words for a given input word
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+word_list = {}
+for i in range(len(data)):
+    word_list[data.letter[i]] = data.code[i]
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+
+def generate_phonetic():
+    output = input("Enter a word: ").upper()
+    try:
+        final_output = [word_list[item] for item in output]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(final_output)
+
+
+generate_phonetic()
+
+# Below code is an alternate method for printing Phonetic Words
+# ----------------------------------------------------------------------------------
+final_output = []
+output1 = input("Enter a word: ").upper()
+for i in output1:
+    x = word_list[i]
+    final_output.append(x)
+print(final_output)
+
+
+# ----------------------------------------------------------------------------------
+#  Below section of code is an example on how to iterate through dictionary
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    print(key)
+    print(value)
+
+# Creating a DataFrame from Dictionary
+student_data = pd.DataFrame(student_dict)
+print(student_data)
+
+# Loop through data frame
+for (key, value) in student_data.items():
+    print(value)
+
+# Loop through rows of a data frame
+for (index, row) in student_data.iterrows():
+    print(row.score)
+    print(row)
+# ------------------------------------------------------------------------------------
+
