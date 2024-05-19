a6a8d739dac40b387084b1fc9d4d775ae87924e9
AnilNAKR <n.anilkumar7896@gmail.com>
2024-05-20 00:51:30 +0530

    removed Day-37-Habit-Tracker file

--- Day 37 - Habit Tracker Pixela/main.py
+++ Day 37 - Habit Tracker Pixela/main.py
@@ -4,7 +4,7 @@ import requests
 from datetime import datetime
 import os
 
-pixela_endpoint = " https://pixe.la/v1/users"
+pixela_endpoint = "https://pixe.la/v1/users"
 USERNAME = os.environ.get("PIXELA_USER_NAME")
 TOKEN = os.environ.get("PIXELA_TOKEN")
 
@@ -21,9 +21,9 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "mygraph2",
+    "id": "mygraph3",
     "name": "Habit Tracker",
-    "unit": "Hours",
+    "unit": "Minutes",
     "type": "int",
     "color": "momiji",
 }
@@ -35,33 +35,37 @@ headers = {
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
+
 pixel_creation = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}"
 
 today = datetime.now()
-# print(today.strftime("%Y%m%d"))
+print(today.strftime("%Y%m%d"))
 
 pixel_data = {
     'date': today.strftime("%Y%m%d"),
-    'quantity': '20',
+    'quantity': '30',
 }
 
 # Post end point
-# response = requests.post(url=pixel_creation, json=pixel_data, headers=headers)
-# print(response.text)
+response = requests.post(url=pixel_creation, json=pixel_data, headers=headers)
+print(response.text)
 
-# Update End Point
-update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}/{today.strftime('%Y%m%d')}"
+# ----------------------------------------------------------------------------------------------------
 
-new_pixel_data = {
-    'quantity': "10"
-}
-
+# # Update End Point
+# update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}/{today.strftime('%Y%m%d')}"
+#
+# new_pixel_data = {
+#     'quantity': "10"
+# }
+#
 # response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
 # print(response.text)
 
+# --------------------------------------------------------------------------------------------------------
 
-#  Delete a Pixel
-delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}/{today.strftime('%Y%m%d')}"
+# #  Delete a Pixel
+# delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}/{today.strftime('%Y%m%d')}"
 
-response = requests.delete(url=delete_endpoint, headers=headers)
-print(response.text)
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
